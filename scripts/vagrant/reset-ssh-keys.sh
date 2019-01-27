bef5b4bc0b2349f7545ad81fa432b59f80f89dd4
Slach <bloodjazman@gmail.com>
2019-01-27 16:54:43 +0300

    tuning SSH Server connection speed
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- scripts/vagrant/reset-ssh-keys.sh
+++ scripts/vagrant/reset-ssh-keys.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sed -i -e "/UseDNS no/s/^#//" /etc/ssh/sshd_config
+sed -i -e "/GSSAPIAuthentication no/s/^#//" /etc/ssh/sshd_config
+
 # Reset to vagrant insecure key
 curl -s -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
 systemctl poweroff
