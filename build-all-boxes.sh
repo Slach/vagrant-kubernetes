5e5e7d979a16f2e47fcb72c0aacd9ac0d462903d
Slach <bloodjazman@gmail.com>
2019-08-10 10:33:21 +0300

    bumb k9s to 0.8.0, exclude cri-o, wait when resolve https://github.com/cri-o/cri-o/issues/2696 and https://github.com/cri-o/cri-o/issues/2695
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- build-all-boxes.sh
+++ build-all-boxes.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -exuv -o pipefail
 
+# @TODO  
 # declare -a ALL_CRI=("containerd" "crio" "docker")
-declare -a ALL_CRI=("crio" "docker")
+declare -a ALL_CRI=("containerd" "docker")
 for USE_CRI in "${ALL_CRI[@]}"
 do
     export RELEASE_DATE=${RELEASE_DATE:-$(date +%Y%m%d%H%M%S)}
--- versions.sh
+++ versions.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export USE_CRI=${USE_CRI:-docker} # avaiable varants "docker", "crio", "containerd"
 export K8S_VERSION=${K8S_VERSION:-1.15}
-export CRIO_VERSION=${CRIO_VERSION:-1.14}
+export CRIO_VERSION=${CRIO_VERSION:-1.15}
 export CONTAINERD_VERSION=${CONTAINERD_VERSION:-1.2.6}
 export IMG_VERSION=${IMG_VERSION:-0.5.7}
 export LOCAL_ETCD=${LOCAL_ETCD:-False}
-export K9S_VERSION=${K9S_VERSION:-0.7.13}
+export K9S_VERSION=${K9S_VERSION:-0.8.0}
