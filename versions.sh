a23c0588195fb44bcb95d76f0015d896af7e440b
Slach <bloodjazman@gmail.com>
2020-01-02 10:22:53 +0500

    update k9s version to 0.10.5
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- versions.sh
+++ versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export USE_CRI=${USE_CRI:-docker} # avaiable varants "docker", "crio", "containerd"
-export K8S_VERSION=${K8S_VERSION:-1.16.2}
+export K8S_VERSION=${K8S_VERSION:-1.17.0}
 export CRIO_VERSION=${CRIO_VERSION:-1.15}
 export CONTAINERD_VERSION=${CONTAINERD_VERSION:-1.2.10}
 export IMG_VERSION=${IMG_VERSION:-0.5.7}
-export K9S_VERSION=${K9S_VERSION:-0.9.2}
+export K9S_VERSION=${K9S_VERSION:-0.10.5}
